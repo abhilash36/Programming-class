7033902db2b1ffd53858528f87c98dafdd7c3669
Admin <Admin@DESKTOP-4VSMM3J>
2019-10-14 13:13:50 +0530

    updated the file hello world by adding the static method m1

--- ProgrammingClass/src/strings/HelloWorld.java
+++ ProgrammingClass/src/strings/HelloWorld.java
@@ -1,7 +1,12 @@
 package strings;
 
-public class HelloWorld {
+public class HelloWorld 
+{
 
+	public static void m1()
+	{
+		System.out.println("This is static method");
+	}
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World");
